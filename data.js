e699c5963d473f71b7bed00715692f281633e560
yureitetsuya <jointhecircle4@gmail.com>
2025-04-03 21:22:52 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+// Hier könnten später komplexere Dienste oder Busdaten rein
+// Beispiel: Dienstangebote mit Namen, Dauer, Einnahmen
+
+const serviceList = [
+  { name: "Kurzdienst", duration: 2, income: 200 },
+  { name: "Mitteldienst", duration: 4, income: 500 },
+  { name: "Langdienst", duration: 6, income: 1000 }
+];
+
+// Beispiel für Busdaten (für spätere Erweiterung)
+const busModels = [
+  { name: "Citaro C2", condition: 100, maintenanceCost: 100 },
+  { name: "MAN Lion's City", condition: 100, maintenanceCost: 120 }
+];
