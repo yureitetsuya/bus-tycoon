dea590d3459b645d777cd3314facdf472fccfedb
yureitetsuya <jointhecircle4@gmail.com>
2025-04-03 22:19:50 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,4 @@
-// Hier könnten später komplexere Dienste oder Busdaten rein
-// Beispiel: Dienstangebote mit Namen, Dauer, Einnahmen
+// Datenstruktur für spätere Inhalte
+// Beispiel: Dienste, Busmodelle, Events etc.
 
-const serviceList = [
-  { name: "Kurzdienst", duration: 2, income: 200 },
-  { name: "Mitteldienst", duration: 4, income: 500 },
-  { name: "Langdienst", duration: 6, income: 1000 }
-];
-
-// Beispiel für Busdaten (für spätere Erweiterung)
-const busModels = [
-  { name: "Citaro C2", condition: 100, maintenanceCost: 100 },
-  { name: "MAN Lion's City", condition: 100, maintenanceCost: 120 }
-];
+// aktuell leer
