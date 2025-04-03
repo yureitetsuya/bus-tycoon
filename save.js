9550da451237809509592e8fce3d57c0ffd236c0
yureitetsuya <jointhecircle4@gmail.com>
2025-04-03 22:35:04 +0200

    Update save.js

--- save.js
+++ save.js
@@ -1,12 +1,1 @@
-function saveGame() {
-  localStorage.setItem("bus-money", money);
-  localStorage.setItem("bus-condition", busCondition);
-}
 
-function loadGame() {
-  const savedMoney = localStorage.getItem("bus-money");
-  const savedCondition = localStorage.getItem("bus-condition");
-
-  money = savedMoney !== null ? parseInt(savedMoney) : 1000;
-  busCondition = savedCondition !== null ? parseInt(savedCondition) : 100;
-}
